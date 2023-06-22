2403f7ce9d3787b2854a9a4975d38a19862cf8c1
ck-siddharth <siddharth.s@codeking.in>
2023-06-22 18:44:30 +0530

    embed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import EmbedComponent from './EmbedComponent';
 
 function App() {
+  const dataId = "001"; // Replace with your desired data-id value
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <EmbedComponent dataId={dataId} />
     </div>
   );
 }
---
+++ src/EmbedComponent.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+class EmbedComponent extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: {
+        "001": [
+          "Heading 1",
+          "Heading 2",
+          "Heading 3",
+          "Heading 4",
+          "Heading 5"
+        ],
+        "002": [
+          "Title 1",
+          "Title 2",
+          "Title 3",
+          "Title 4",
+          "Title 5"
+        ]
+      }
+    };
+  }
+
+  render() {
+    const { data } = this.state;
+    const { dataId } = this.props;
+    const headings = data[dataId];
+
+    if (!headings) return null;
+
+    return (
+      <>
+        <div id="headings" data-id={dataId}></div>
+        <ul>
+          {headings.map((heading, index) => (
+            <li key={index}>{heading}</li>
+          ))}
+        </ul>
+      </>
+    );
+  }
+}
+
+export default EmbedComponent;
